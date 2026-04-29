7605c210fbc2018d257dd95bcd12890db8935983
MORPALMAURYA <morpalmaurya2003@gmail.com>
2026-04-29 18:31:13 +0530

    fix lombok build error

--- src/main/java/com/morpal/MorpalFoodApplication.java
+++ src/main/java/com/morpal/MorpalFoodApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ZoshFoodApplication {
+public class MorpalFoodApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZoshFoodApplication.class, args);
+		SpringApplication.run(MorpalFoodApplication.class, args);
 	}
 
 }
--- src/test/java/com/morpal/MorpalFoodApplicationTests.java
+++ src/test/java/com/morpal/MorpalFoodApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ZoshFoodApplicationTests {
+class MorpalFoodApplicationTests {
 
 	@Test
 	void contextLoads() {
